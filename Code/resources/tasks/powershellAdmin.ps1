fbee90f5c49252f45462d17fdb39d8c06d3a3f51
Evan Stoller <163201695+Pokevan8162@users.noreply.github.com>
2025-08-13 11:58:53 -0400

    Delete Code directory

--- Code/resources/tasks/powershellAdmin.ps1
+++
@@ -1,5 +0,0 @@
-# Automatically run the script in admin
-
-$scriptPath = Join-Path -Path $PSScriptRoot -ChildPath "create_observe_user_task.ps1"
-
-Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
--- Code/resources/tasks/run_observe_user_activity.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-cd ..
-cd ..
-php artisan app:observe-user-activity
